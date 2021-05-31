7e4e0620fb6d35d27bb9a4e78356ed8a4cc4972c
yogi-masaji <yogimasaji01@gmail.com>
2021-05-31 23:32:24 +0700

    add text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HelloWorld(props) {
+  return <div>{props.kata}</div>
+  
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <HelloWorld kata="heloooooo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!!
         </p>
         <a
           className="App-link"
